798ef3a3852c6e9231e85b1149b63fe83d429984
ezraeffect <build@fx0.kr>
2025-04-30 09:59:43 +0900

    add:input textBox

--- sfs_day8_coinFlipping/day8/Form1.Designer.cs
+++ sfs_day8_coinFlipping/day8/Form1.Designer.cs
@@ -28,23 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox_print
+            // textBox_result
             // 
-            this.textBox_print.Location = new System.Drawing.Point(12, 12);
-            this.textBox_print.Multiline = true;
-            this.textBox_print.Name = "textBox_print";
-            this.textBox_print.Size = new System.Drawing.Size(776, 426);
-            this.textBox_print.TabIndex = 0;
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Location = new System.Drawing.Point(12, 39);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(776, 399);
+            this.textBox_result.TabIndex = 2;
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(12, 12);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(695, 21);
+            this.textBox_input.TabIndex = 0;
+            // 
+            // button_input
+            // 
+            this.button_input.Location = new System.Drawing.Point(713, 10);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(75, 23);
+            this.button_input.TabIndex = 1;
+            this.button_input.Text = "Input";
+            this.button_input.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox_print);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.textBox_result);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +76,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox_print;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Button button_input;
     }
 }
 
