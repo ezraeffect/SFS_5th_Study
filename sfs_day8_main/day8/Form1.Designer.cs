35991fdff761af19d576d96f2c617ca300b701f1
ezraeffect <build@fx0.kr>
2025-04-30 10:29:26 +0900

    add:동전던지기

--- sfs_day8_main/day8/Form1.Designer.cs
+++ sfs_day8_main/day8/Form1.Designer.cs
@@ -31,16 +31,20 @@
             this.textBox_result = new System.Windows.Forms.TextBox();
             this.textBox_input = new System.Windows.Forms.TextBox();
             this.button_input = new System.Windows.Forms.Button();
+            this.radioButtonFront = new System.Windows.Forms.RadioButton();
+            this.radioButtonBack = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox_result
             // 
             this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.textBox_result.Location = new System.Drawing.Point(12, 39);
+            this.textBox_result.Location = new System.Drawing.Point(12, 61);
             this.textBox_result.Multiline = true;
             this.textBox_result.Name = "textBox_result";
             this.textBox_result.ReadOnly = true;
-            this.textBox_result.Size = new System.Drawing.Size(776, 399);
+            this.textBox_result.Size = new System.Drawing.Size(776, 377);
             this.textBox_result.TabIndex = 2;
             // 
             // textBox_input
@@ -58,6 +62,38 @@
             this.button_input.TabIndex = 1;
             this.button_input.Text = "Input";
             this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // radioButtonFront
+            // 
+            this.radioButtonFront.AutoSize = true;
+            this.radioButtonFront.Location = new System.Drawing.Point(3, 3);
+            this.radioButtonFront.Name = "radioButtonFront";
+            this.radioButtonFront.Size = new System.Drawing.Size(47, 16);
+            this.radioButtonFront.TabIndex = 3;
+            this.radioButtonFront.TabStop = true;
+            this.radioButtonFront.Text = "앞면";
+            this.radioButtonFront.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBack
+            // 
+            this.radioButtonBack.AutoSize = true;
+            this.radioButtonBack.Location = new System.Drawing.Point(56, 3);
+            this.radioButtonBack.Name = "radioButtonBack";
+            this.radioButtonBack.Size = new System.Drawing.Size(47, 16);
+            this.radioButtonBack.TabIndex = 4;
+            this.radioButtonBack.TabStop = true;
+            this.radioButtonBack.Text = "뒷면";
+            this.radioButtonBack.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButtonFront);
+            this.panel1.Controls.Add(this.radioButtonBack);
+            this.panel1.Location = new System.Drawing.Point(12, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 27);
+            this.panel1.TabIndex = 5;
             // 
             // Form1
             // 
@@ -67,8 +103,11 @@
             this.Controls.Add(this.button_input);
             this.Controls.Add(this.textBox_input);
             this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +118,9 @@
         private System.Windows.Forms.TextBox textBox_result;
         private System.Windows.Forms.TextBox textBox_input;
         private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.RadioButton radioButtonFront;
+        private System.Windows.Forms.RadioButton radioButtonBack;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
