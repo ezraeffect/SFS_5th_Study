fc09f27cb9d4e5b73770b96fe259fdbb763946bd
ezraeffect <build@fx0.kr>
2025-05-09 10:56:45 +0900

    실습:학생 정보 관리

--- sfs_day13_main/day13/Form1.cs
+++ sfs_day13_main/day13/Form1.cs
@@ -11,41 +11,46 @@ using System.Windows.Forms;
 
 namespace day13
 {
-    // 실습. 좌표(Point) 구조체 만들기
-    // 1. x, y 좌표값을 가지는 Point 구조체를 만든다
-    // 2. 사용자에게 좌표 두 개를 입력는다
-    // 3. 두 점 사이의 거리 를 구하세요.
+    // 실습. 학생(Student) 정보 관리
+    // 1. 학생 정보를 담는 구조체를 정의
+    // 2. 3명의 학생 정보를 입력 받아 출력
+    // * 한 줄 코드는 표현식 본문 방법으로 처리
 
-    struct Point
+    // 요구사항
+    // - 구조체 이름 : Student
+    // - 변수 : string name, int age, double score
+    // - 메서드 : 입력받은 학생들의 정보를 출력
+
+    struct Student
     {
-        public int x;
-        public int y;
+        public string name;
+        public int age;
+        public double score;
+
+        public void PrintStudentInfo () => Console.WriteLine ($"{name} 학생의 나이는 {age}살이며 성적은 {score}점 입니다.");
     }
 
     public partial class Form1 : Form
     {
-        static double GetBetweenDistance(int x, int y, int x2, int y2) => Math.Sqrt(Math.Pow(x2 - x, 2) + Math.Pow(y2 - y, 2));
-
         public Form1()
         {
             InitializeComponent();
 
-            while (true)
-            {
-                Point[] p = new Point[2];
+            Student[] students = new Student[3];
 
-                for (int i = 0; i < 2; i++)
-                {
-                    Console.WriteLine("X좌표와 Y좌표를 입력하세요.");
-                    string[] inputStr = Console.ReadLine().Split(' ');
-                    if (inputStr.Length == 2)
-                    {
-                        if (!int.TryParse(inputStr[0], out p[i].x)) Console.WriteLine("인자값의 형식이 잘못되었습니다");
-                        if (!int.TryParse(inputStr[1], out p[i].x)) Console.WriteLine("인자값의 형식이 잘못되었습니다");
-                    }
-                    else Console.WriteLine("인자값의 갯수가 잘못되었습니다");
-                }
-                Console.WriteLine($"두 점 사이의 거리는 {GetBetweenDistance(p[0].x, p[0].y, p[1].x, p[1].y)}");
+            for ( int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{i + 1} 번째 학생의 이름을 입력하세요.");
+                students[i].name = Console.ReadLine();
+                Console.WriteLine($"{i + 1} 번째 학생의 나이를 입력하세요.");
+                students[i].age = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{i + 1} 번째 학생의 성적을 입력하세요.");
+                students[i].score = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                students[i].PrintStudentInfo();
             }
         }
     }
