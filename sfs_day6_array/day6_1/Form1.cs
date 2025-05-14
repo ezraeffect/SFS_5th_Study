e15ed2cd486367b6ed6006a2141cbace92d1567a
Jeonghun Lee <build@fx0.kr>
2025-05-14 16:19:49 +0900

    add:array 실습

--- sfs_day6_array/day6_1/Form1.cs
+++ sfs_day6_array/day6_1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace day6_1
 {
@@ -16,30 +17,25 @@ namespace day6_1
         {
             InitializeComponent();
 
-            // Q1. 과일 이름, 가격, 수량을 변수에 저장하고 총 가격을 계산하여 출력한다.
-            string fruitType = "apple";
-            int fruitPrice = 1500;
-            int fruitQuantity = 10;
-            int totalPrice = fruitPrice * fruitQuantity;
+            string[,] korean = {{"가","나","다"},
+                                {"라","마","바"},
+                                {"사","아","자"}};
 
-            textBox_print.Text = $"{fruitType} {fruitQuantity}개의 총 가격은 {totalPrice}원 입니다.\r\n";
+            textBox_print.Text = korean[1, 2];
 
-            // Q2. 문자열 + 숫자
-            string q2str = "15";
-            textBox_print.Text += $"{q2str}에 10을 더하면 {int.Parse(q2str) + 10} 입니다.\r\n";
-                    
-            // Q3. 종합 문제
-            string goodsName = "노트북";
-            int goodsPrice = 1200000;
-            float goodsDiscount = 0.15f;
-            byte goodsQuantity = 8;
-            bool isAvailable = (goodsQuantity >= 1) ? true : false;
-            double discountedPrice = goodsPrice * (1 - goodsDiscount);
+            int[,,] nums = new int[,,]
+            {
+                {
+                    {1, 2, 3},
+                    {4, 5, 6}
+                },
+                {
+                    {7, 8, 9},
+                    {10, 11, 12}
+                }
+            };
 
-            textBox_print.Text += $"{(isAvailable ? "구매가능 : 할인가격은" + discountedPrice.ToString() + "원 입니다." : "품절되었습니다")}\r\n";
-            textBox_print.Text += $"{((goodsQuantity >= 5) ? "여유 있음" : "소량 남음")}\r\n";
-            textBox_print.Text += $"상품명 : {goodsName}, 할인된 가격 : {discountedPrice}, 재고 : {goodsQuantity}";
-
+            textBox_print.Text += nums[1, 0, 1];
         }
     }
 }
