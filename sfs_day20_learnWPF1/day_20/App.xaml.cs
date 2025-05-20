e12fd804bcd032c12d02de12fd62f4072e8a8f7e
ezraeffect <build@fx0.kr>
2025-05-20 13:17:26 +0900

    Change: from WinForm to WPF

---
+++ sfs_day20_learnWPF1/day_20/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace day_20
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
