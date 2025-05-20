fa662d3c57209bd39caa2c98247f5d0a3b06504c
ezraeffect <build@fx0.kr>
2025-05-20 12:23:34 +0900

    add: await, async 실습

--- sfs_day20_taskAsyncAwaitPractice/day_20/Form1.Designer.cs
+++ sfs_day20_taskAsyncAwaitPractice/day_20/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.textBox_filePath = new System.Windows.Forms.TextBox();
+            this.button_OpenDialog = new System.Windows.Forms.Button();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Location = new System.Drawing.Point(12, 12);
+            this.textBox_Result.Multiline = true;
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.Size = new System.Drawing.Size(776, 397);
+            this.textBox_Result.TabIndex = 0;
+            // 
+            // textBox_filePath
+            // 
+            this.textBox_filePath.Location = new System.Drawing.Point(12, 417);
+            this.textBox_filePath.Name = "textBox_filePath";
+            this.textBox_filePath.ReadOnly = true;
+            this.textBox_filePath.Size = new System.Drawing.Size(658, 21);
+            this.textBox_filePath.TabIndex = 1;
+            // 
+            // button_OpenDialog
+            // 
+            this.button_OpenDialog.Location = new System.Drawing.Point(676, 415);
+            this.button_OpenDialog.Name = "button_OpenDialog";
+            this.button_OpenDialog.Size = new System.Drawing.Size(31, 23);
+            this.button_OpenDialog.TabIndex = 2;
+            this.button_OpenDialog.Text = "...";
+            this.button_OpenDialog.UseVisualStyleBackColor = true;
+            this.button_OpenDialog.Click += new System.EventHandler(this.button_OpenDialog_Click);
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(713, 415);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 23);
+            this.button_Start.TabIndex = 3;
+            this.button_Start.Text = "시작";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.button_OpenDialog);
+            this.Controls.Add(this.textBox_filePath);
+            this.Controls.Add(this.textBox_Result);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.TextBox textBox_filePath;
+        private System.Windows.Forms.Button button_OpenDialog;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- sfs_day20_taskAsyncAwaitPractice/day_20/Form1.cs
+++ sfs_day20_taskAsyncAwaitPractice/day_20/Form1.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * • Task, async, await을 사용하여 아래 기능을 구현
+ * • 버튼을 클릭하여 텍스트 파일을 선택하면, 파일의 내용을 텍스트박스에 표시해주는 프로그램
+ *  1. 텍스트 파일을 비동기적으로 읽어오는 ReadFileAsync 메소드 작성
+ *  2. StreamReader를 이용하여 파일의 내용을 불러오고, StreamReader에 내장된 ReadToEndAsync() 메소드를 사용하여 읽어오는 부분도 비동기처리
+ */
 namespace day_20
 {
     public partial class Form1 : Form
@@ -15,6 +21,51 @@ namespace day_20
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async Task ReadFileAsync(string path)
+        {
+            if (!string.IsNullOrWhiteSpace(path) || File.Exists(path))
+            {
+                try
+                {
+                    // Create an instance of StreamReader to read from a file.
+                    // The using statement also closes the StreamReader.
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        textBox_Result.AppendText(await sr.ReadToEndAsync());
+                    }
+                }
+                catch (Exception e)
+                {
+                    // Let the user know what went wrong.
+                    textBox_Result.AppendText("\r\nThe file could not be read:");
+                    textBox_Result.AppendText(e.Message);
+                }
+            }
+            else
+            {
+                textBox_Result.AppendText("파일 경로가 잘못되었습니다");
+            }
+        }
+
+        private void button_OpenDialog_Click(object sender, EventArgs e)
+        {
+            DialogResult readResult = openFileDialog1.ShowDialog();
+            if (readResult == DialogResult.OK)
+            {
+                textBox_filePath.Text = openFileDialog1.FileName;
+            }
+            else { textBox_filePath.Text = string.Empty; }
+        }
+
+        private async void button_Start_Click(object sender, EventArgs e)
+        {
+            string filePath = textBox_filePath.Text;
+
+            await ReadFileAsync(filePath);
+
+            textBox_Result.AppendText("\r\n읽기 완료");
         }
     }
 }
