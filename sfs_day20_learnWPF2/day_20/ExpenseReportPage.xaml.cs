76ed5654dd442a2e9aa9f100c52ee5e1cfeaf4ad
ezraeffect <build@fx0.kr>
2025-05-21 10:02:18 +0900

    자습서: Visual Studio 2019에서 첫 번째 WPF 애플리케이션 만들기

--- sfs_day20_learnWPF2/day_20/ExpenseReportPage.xaml.cs
+++ sfs_day20_learnWPF2/day_20/ExpenseReportPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,13 +16,18 @@ using System.Windows.Shapes;
 namespace day_20
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// ExpenseReportPage.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class ExpenseReportPage : Page
     {
-        public MainWindow()
+        public ExpenseReportPage()
         {
             InitializeComponent();
+        }
+
+        public ExpenseReportPage(object data):this()
+        {
+            this.DataContext = data;
         }
     }
 }
